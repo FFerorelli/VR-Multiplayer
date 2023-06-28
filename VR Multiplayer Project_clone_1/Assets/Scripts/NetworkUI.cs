05bace915ad47439d9af2188b72a6a231ac7e260
FFerorelli <fabrizio.ferorelli@gmail.com>
2023-06-28 18:03:11 +0200

    fix

---
+++ VR Multiplayer Project_clone_1/Assets/Scripts/NetworkUI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Unity.Netcode;
+
+public class NetworkUI : MonoBehaviour
+{
+    public Button hostButton;
+    public Button serverButtton;
+    public Button clientButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hostButton.onClick.AddListener(() => NetworkManager.Singleton.StartHost());
+        serverButtton.onClick.AddListener(() => NetworkManager.Singleton.StartServer());
+        clientButton.onClick.AddListener(() => NetworkManager.Singleton.StartClient());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
